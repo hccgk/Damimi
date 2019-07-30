1d1f179e145524dd8d828913c7d31d38675a9b2d
hechuan <hccgk@sina.com>
2019-07-30 17:14:18 +0800

    init Pro

---
+++ Damimi/Damimi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Damimi
+//
+//  Created by hechuan on 2019/7/30.
+//  Copyright © 2019 hechuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
